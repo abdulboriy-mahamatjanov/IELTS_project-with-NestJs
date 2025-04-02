9ae23637b3bd11b8018ad983e15674604f4a4dfb
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-02 22:04:26 +0600

    src folder has updated with a new files and functions, swaggers, apioperations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { RegionModule } from './region/region.module';
+import { UploadModule } from './upload/upload.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), PrismaModule, RegionModule, UploadModule, UserModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('The IELTS mock exam project')
+    .setDescription('The IELTS mock exam project description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3000, () =>
+    console.log(`Server started on port: ${process.env.PORT}`),
+  );
 }
+
 bootstrap();
